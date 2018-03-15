1ba69de1795d558e9fb3e006aaa6ec22551a11cb
Fred <Fred@CreativeProjects.Tech>
2018-03-15 14:47:27 +0000

    Moved from php-git-ci and changed tag to php-ci

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+image_name=creativeprojects/php-ci
+image_versions="5.6 7.0 7.1 7.2"
+
+cd $(dirname "${0}")
+
+for image_version in ${image_versions}; do
+    echo Downloading php:${image_version}
+    docker pull php:${image_version}
+
+    # docker rmi ${image_name}:${image_version}
+    # docker rmi ${image_name}:latest
+
+    echo Building image ${image_name}:${image_version}
+    docker build -t ${image_name}:${image_version} \
+                 -t ${image_name}:latest \
+                 -f Dockerfile-php${image_version} \
+                 ./
+
+    echo Pushing image ${image_name}:${image_version}
+    docker push ${image_name}:${image_version}
+
+    echo Pushing image ${image_name}:latest
+    docker push ${image_name}:latest
+done
